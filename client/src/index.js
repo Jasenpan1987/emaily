03e1eaa0f731be75958b59b1a178001fcf5f4b80
Jasenpan1987 <jasenpan@gmail.com>
2017-08-13 18:26:10 +1000

    redux setup and route setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from "./components/App";
+import rootReducer from "./reducers";
+// remove package-lock.json in client directory
+// run npm install
+
+const store = createStore(rootReducer, {}, applyMiddleware());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector("#root")
+);
