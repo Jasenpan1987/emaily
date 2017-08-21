8b0ef1accb9db6c3d4187c7c0717106f583c9f65
Jasenpan1987 <jasenpan@gmail.com>
2017-08-21 22:19:28 +1000

    dashboard back end

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,8 +10,12 @@ const requireCredit = require("../middlewares/requireCredit");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-    app.get("/api/surveys/thanks", (req, res) => {
-        res.send("Thanks for voting.");
+   
+    app.get("/api/surveys", requireLogin, async (req, res) => {
+        const surveys = await Survey.find({ _user: req.user.id })
+            .select({ recipients: false });
+
+        res.send(surveys);
     });
 
     // create new survey
@@ -21,8 +25,8 @@ module.exports = app => {
         async (req, res) => {
             const { title, body, subject, recipients } = req.body;
             const recipientEmailList = recipients
-                .replace(/' '/g, '')
                 .split(',')
+                .trim()
                 .map(email => ({ email }));
 
             const survey = new Survey({
@@ -48,10 +52,20 @@ module.exports = app => {
         }
     );
 
+    app.get("/api/surveys/thanks", (req, res) => {
+        res.send("Thanks for voting.");
+    });
+
+    app.get("/api/surveys/:surveyId/:choice", (req, res) => {
+        res.send("Thanks for voting...");
+    });
+
     app.post("/api/surveys/webhooks", (req, res) => {
         const p = new Path("/api/surveys/:surveyId/:choice");
-        const events = _.chain(req.body)
+        // we don't have to respond anything so no async this time
+        _.chain(req.body)
             .map(({ email, url}) => {
+                console.log(email);
                 const matched = p.test(new URL(url).pathname);
                 if(matched) {
                     return { email, surveyId: matched.surveyId, choice: matched.choice};
@@ -59,9 +73,20 @@ module.exports = app => {
             })
             .compact()
             .uniqBy('email', 'surveyId')
+            .each(({ email, surveyId, choice }) => {
+                Survey.findOneAndUpdate({
+                    _id: surveyId,
+                    recipients: {
+                        $elemMatch: { email, responded: false }
+                    }
+                }, {
+                    $inc: { [choice]: 1 },
+                    $set: { "recipients.$.responded": true, lastResponded: Date.now() }
+                }).exec((result) => {
+                    console.log("a result coming in for " + result);
+                });
+            })
             .value();
-        
-        console.log(events);
         res.send({});
     });
 }
